f4f74a21f1ce4b3baa7491d8fc04e4e91cd8ac7a
Joseph Venable <JJVenable@gmail.com>
2022-02-08 15:51:17 -0500

    serializers.py

--- backend_django/django2/serializers.py
+++ backend_django/django2/serializers.py
@@ -6,7 +6,11 @@ class TruckSerializer(serializers.HyperlinkedModelSerializer):
         view_name='job_detail',
         many=True,
         read_only=True
-    tools = serializers.HyperlinkedRelatedField()
+    )
+    tools = serializers.HyperlinkedRelatedField(
+        view_name='tool_detail',
+        many=True,
+        read_only=True
     )
     class Meta: 
       model = Truck
@@ -21,4 +25,15 @@ class ToolSerializer(serializers.HyperlinkedModelSerializer):
     class Meta: 
       model = Tool
       fields = ('name', 'notes')
+  
+  
+class JobSerializer(serializers.HyperlinkedModelSerializer):
+    trucks = serializers.HyperlinkedRelatedField(
+        view_name='truck_detail',
+        many=True,
+        read_only=True
+    )
+    class Meta: 
+      model = Job
+      fields = ('suggested_tools', 'address', 'scheduled', 'comments')
   
--- backend_django/django2/views.py
+++ backend_django/django2/views.py
@@ -1,23 +1,58 @@
-from django.shortcuts import render, redirect
+# from django.shortcuts import render, redirect
+from rest_framework import generics
 from .models import Truck, Tool, Job
+# from .forms import TruckForm, ToolForm, JobForm
+from .serializers import TruckSerializer, ToolSerializer, JobSerializer
+
 # Create your views here.
 # views.py
 from django.http import JsonResponse
 
-def tool_list(request):
-    tools = Tool.objects.all().values('name', 'notes') # only grab some attributes from our database, else we can't serialize it.
-    tools_list = list(tools) # convert our artists to a list instead of QuerySet
-    return JsonResponse(tools_list, safe=False) # safe=False is needed if the first parameter is not a dictionary.
 
-def truck_list(request):
-    trucks = Truck.objects.all().values('number', 'drivers', 'notes') 
-    trucks_list = list(trucks) 
-    return JsonResponse(trucks_list, safe=False)
+class TruckList(generics.ListCreateAPIView):
+    queryset = Truck.objects.all()
+    serializer_class = TruckSerializer
 
-def job_list(request):
-    jobs = Job.objects.all().values('suggested_tools', 'address', 'scheduled', 'comments') 
-    jobs_list = list(jobs) 
-    return JsonResponse(jobs_list, safe=False)
+class TruckDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Truck.objects.all()
+    serializer_class = TruckSerializer
 
-def job_detail
-def truck_detail
+class ToolList(generics.ListCreateAPIView):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
+
+class ToolDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
+
+class JobList(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+
+
+
+
+
+# def tool_list(request):
+#     tools = Tool.objects.all().values('name', 'notes') 
+#     tools_list = list(tools) 
+#     return JsonResponse(tools_list, safe=False) 
+
+# def truck_list(request):
+#     trucks = Truck.objects.all().values('number', 'drivers', 'notes') 
+#     trucks_list = list(trucks) 
+#     return JsonResponse(trucks_list, safe=False)
+
+# def job_list(request):
+#     jobs = Job.objects.all().values('suggested_tools', 'address', 'scheduled', 'comments') 
+#     jobs_list = list(jobs) 
+#     return JsonResponse(jobs_list, safe=False)
+
+# def job_detail
+# def truck_detail
