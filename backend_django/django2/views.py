298571f302e79bab35476b7149e44d3d8d231529
Joseph Venable <JJVenable@gmail.com>
2022-02-08 14:14:22 -0500

    begin views

--- backend_django/django2/views.py
+++ backend_django/django2/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+from django.http import JsonResponse
+
+def tool_list(request):
+    tools = Tool.objects.all().values('name', 'notes') # only grab some attributes from our database, else we can't serialize it.
+    tools_list = list(tools) # convert our artists to a list instead of QuerySet
+    return JsonResponse(tools_list, safe=False) # safe=False is needed if the first parameter is not a dictionary.
