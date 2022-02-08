20a052961343280c9c5b46f567f0f05cc3f94369
Joseph Venable <JJVenable@gmail.com>
2022-02-08 14:34:55 -0500

    truck&job list in views.py

--- backend_django/django2/views.py
+++ backend_django/django2/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Truck, Tool, Job
 # Create your views here.
 # views.py
 from django.http import JsonResponse
@@ -8,3 +8,13 @@ def tool_list(request):
     tools = Tool.objects.all().values('name', 'notes') # only grab some attributes from our database, else we can't serialize it.
     tools_list = list(tools) # convert our artists to a list instead of QuerySet
     return JsonResponse(tools_list, safe=False) # safe=False is needed if the first parameter is not a dictionary.
+
+def truck_list(request):
+    trucks = Truck.objects.all().values('number', 'drivers', 'notes') 
+    trucks_list = list(trucks) 
+    return JsonResponse(trucks_list, safe=False)
+
+def job_list(request):
+    jobs = Job.objects.all().values('suggested_tools', 'address', 'scheduled', 'comments') 
+    jobs_list = list(jobs) 
+    return JsonResponse(jobs_list, safe=False)
